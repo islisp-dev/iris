40ed02a3d48be0220a8888c80bcab3e0b1718ac2
TANIGUCHI Masaya <ta2gch@users.noreply.github.com>
2017-07-24 12:14:59 +0900

    refactor source code

--- object.go
+++ object.go
@@ -10,3 +10,7 @@ type Object struct {
 func NewCons(car *Object, cdr *Object) *Object {
 	return &Object{"cons", car, cdr, nil}
 }
+
+func (o *Object) Is(val interface{}) bool {
+	return o.Val == val
+}
