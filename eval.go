dfea76ce9ce8ecf039c22b67ea82720684a4d209
TANIGUCHI Masaya <ta2gch@users.noreply.github.com>
2017-07-25 17:42:25 +0900

    Changed function arguments

--- eval.go
+++ eval.go
@@ -6,19 +6,23 @@ import (
 )
 
 type Env struct {
-	Fun map[string]func(*Object) (*Object, error)
+	Fun map[string]func(*Object, *Env, *Env) (*Object, error)
 	Var map[string]*Object
 }
 
 func NewEnv() *Env {
 	env := new(Env)
-	env.Fun = map[string]func(*Object) (*Object, error){}
+	env.Fun = map[string]func(*Object, *Env, *Env) (*Object, error){}
 	env.Var = map[string]*Object{}
 	return env
 }
 
 func Eval(obj *Object, local *Env, global *Env) (*Object, error) {
-	if obj.Type == "symbol" {
+	if obj == nil {
+		return nil, nil
+	}
+	switch obj.Type {
+	case "symbol":
 		if val, ok := local.Var[obj.Val.(string)]; ok {
 			return val, nil
 		}
@@ -26,6 +30,10 @@ func Eval(obj *Object, local *Env, global *Env) (*Object, error) {
 			return val, nil
 		}
 		return nil, fmt.Errorf("%s is not defined", obj.Val)
+	case "list":
+		// funcall
+	case "integer", "float", "character", "string":
+		return obj, nil
 	}
 	return nil, errors.New("I have no ideas")
 }
