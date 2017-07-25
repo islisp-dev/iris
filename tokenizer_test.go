98acaf6cec7b3f8ddb12e5428fe1d8664b35c67f
asciian <asciian@outlook.jp>
2017-07-25 14:07:11 +0900

    added unit test

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	"io"
 	"strings"
 	"testing"
 )
 
-func TestReadToken(t *testing.T) {
-	ans := []string{"(", "print", "#\\a", ")", "(", "cons", "'", "a", "'", "(", "1", ".", "1.3E-10", ")", ")"}
-	re := NewReader(strings.NewReader("(print  #\\a) (cons 'a '(1 . 1.3E-10))"))
-	for _, a := range ans {
-		if b, err := re.ReadToken(); err != nil || a != b {
-			t.Errorf("%s is not %s.", b, a)
-		}
+func TestReader_ReadToken(t *testing.T) {
+	type fields struct {
+		err error
+		ru  rune
+		sz  int
+		rr  io.RuneReader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "symbol",
+			fields:  fields{nil, 'd', 8, strings.NewReader("efault")},
+			want:    "default",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Reader{
+				err: tt.fields.err,
+				ru:  tt.fields.ru,
+				sz:  tt.fields.sz,
+				rr:  tt.fields.rr,
+			}
+			got, err := r.ReadToken()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Reader.ReadToken() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Reader.ReadToken() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
