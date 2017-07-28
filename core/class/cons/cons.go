0a65b552b75dc977767fb214bc0d31ddf8bb86a1
TANIGUCHI Masaya <ta2gch@users.noreply.github.com>
2017-07-28 13:18:00 +0900

    Used Null class

--- core/class/cons/cons.go
+++ core/class/cons/cons.go
@@ -8,24 +8,27 @@ import (
 
 // Cell is a pair of pointers to Object
 type Cell struct {
-	Car *class.Instance
-	Cdr *class.Instance
+	car *class.Instance
+	cdr *class.Instance
 }
 
 func New(car *class.Instance, cdr *class.Instance) *class.Instance {
+	if cdr.Class() == class.Null || cdr.Class() == class.List {
+		return class.List.New(&Cell{car, cdr})
+	}
 	return class.Cons.New(&Cell{car, cdr})
 }
 
 func Car(i *class.Instance) (*class.Instance, error) {
-	if i == nil || i.Class() != class.Cons && i.Class() != class.List {
+	if i.Class() == class.Null || (i.Class() != class.Cons && i.Class() != class.List) {
 		return nil, fmt.Errorf("%v is not a member of <cons>", i.Class().ToString())
 	}
-	return i.Value().(Cell).Car, nil
+	return i.Value().(*Cell).car, nil
 }
 
 func Cdr(i *class.Instance) (*class.Instance, error) {
-	if i == nil || i.Class() != class.Cons && i.Class() != class.List {
+	if i.Class() == class.Null || (i.Class() != class.Cons && i.Class() != class.List) {
 		return nil, fmt.Errorf("%v is not a member of <cons>", i.Class().ToString())
 	}
-	return i.Value().(Cell).Cdr, nil
+	return i.Value().(*Cell).cdr, nil
 }
