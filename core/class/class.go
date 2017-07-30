506d4434a72289aaf88527739f3a51406c66252b
TANIGUCHI Masaya <ta2gch@users.noreply.github.com>
2017-07-30 13:41:59 +0900

    Changed type of Parents

--- core/class/class.go
+++ core/class/class.go
@@ -2,7 +2,7 @@ package class
 
 type Class interface {
 	ToString() string
-	Parents() []Class
+	Parents() []Instance
 	New(...Value) Instance
 }
 
@@ -10,91 +10,91 @@ func New(c Instance, value ...Value) Instance {
 	return c.(Class).New(value...)
 }
 
-type MetaClass struct {
+type meta struct {
 	name string
 }
 
-func (m *MetaClass) ToString() string {
+func (m *meta) ToString() string {
 	return m.name
 }
 
-func (*MetaClass) Parents() []Class {
-	return []Class{Object.Value().(Class)}
+func (*meta) Parents() []Instance {
+	return []Instance{Object}
 }
 
-func (*MetaClass) Class() Instance {
-	return Object
-}
-
-func (m *MetaClass) Value() Value {
-	return m
-}
-
-func (m *MetaClass) New(value ...Value) Instance {
-	return &builtInClass{[]Class{value[0].(Class)}, value[1].(string)}
-}
-
-type builtInClass struct {
-	parents []Class
-	name    string
-}
-
-func (c *builtInClass) ToString() string {
-	return c.name
-}
-
-func (c *builtInClass) Parents() []Class {
-	return c.parents
-}
-
-func (c *builtInClass) Class() Instance {
+func (*meta) Class() Instance {
 	return BuiltInClass
 }
 
-func (c *builtInClass) Value() Value {
+func (m *meta) Value() Value {
+	return m
+}
+
+func (m *meta) New(value ...Value) Instance {
+	return &builtin{[]Instance{value[0].(Instance)}, value[1].(string)}
+}
+
+type builtin struct {
+	parents []Instance
+	name    string
+}
+
+func (c *builtin) ToString() string {
+	return c.name
+}
+
+func (c *builtin) Parents() []Instance {
+	return c.parents
+}
+
+func (c *builtin) Class() Instance {
+	return BuiltInClass
+}
+
+func (c *builtin) Value() Value {
 	return c
 }
 
-func (c *builtInClass) New(value ...Value) Instance {
+func (c *builtin) New(value ...Value) Instance {
 	return &DefaultInstance{c, value[0]}
 }
 
-var Object = &builtInClass{[]Class{}, "<object>"}
-var BasicArray = BuiltInClass.New(Object, "<basic-array>")
-var BasicArrayStar = BuiltInClass.New(BasicArray, "<basic-array*>")
-var GeneralArrayStar = BuiltInClass.New(BasicArrayStar, "<general-array*>")
-var BasicVector = BuiltInClass.New(BasicArray, "<basic-vector>")
-var GeneraVector = BuiltInClass.New(BasicVector, "<general-vector>")
-var String = BuiltInClass.New(BasicVector, "<string>")
-var BuiltInClass = &MetaClass{"<built-in-class>"}
-var Character = BuiltInClass.New(Object, "<character>")
-var Function = BuiltInClass.New(Object, "<function>")
-var GenericFunction = BuiltInClass.New(Function, "<generic-function>")
-var StandardGenericFunction = BuiltInClass.New(GenericFunction, "<standard-generic-function>")
-var List = BuiltInClass.New(Object, "<list>")
-var Cons = BuiltInClass.New(List, "<cons>")
-var Null = BuiltInClass.New(List, "<null>")
-var Symbol = BuiltInClass.New(Object, "<symbol>")
-var Number = BuiltInClass.New(Object, "<number>")
-var Integer = BuiltInClass.New(Number, "<integer>")
-var Float = BuiltInClass.New(Number, "<float>")
-var SeriousCondition = BuiltInClass.New(Object, "<serious-condition>")
-var Error = BuiltInClass.New(SeriousCondition, "<error>")
-var ArithmeticError = BuiltInClass.New(Error, "<arithmetic-error>")
-var DivisionByZero = BuiltInClass.New(ArithmeticError, "<division-by-zero>")
-var FloatingPointOnderflow = BuiltInClass.New(ArithmeticError, "<floating-point-overflow>")
-var FloatingPointUnderflow = BuiltInClass.New(ArithmeticError, "<floating-point-underflow>")
-var ControlError = BuiltInClass.New(Error, "<control-error>")
-var ParseError = BuiltInClass.New(Error, "<parse-error>")
-var ProgramError = BuiltInClass.New(Error, "<program-error>")
-var DomainError = BuiltInClass.New(ProgramError, "<domain-error>")
-var UndefinedEntity = BuiltInClass.New(ProgramError, "<undefined-entity>")
-var UndefinedVariable = BuiltInClass.New(UndefinedEntity, "<undefined-variable>")
-var UndefinedFunction = BuiltInClass.New(UndefinedEntity, "<undefined-function>")
-var SimpleError = BuiltInClass.New(Error, "<simple-error>")
-var StreamError = BuiltInClass.New(Error, "<stream-error>")
-var EndOfStream = BuiltInClass.New(StreamError, "<end-of-stream>")
-var StorageExhausted = BuiltInClass.New(SeriousCondition, "<storage-exhausted>")
-var StandardClass = &MetaClass{"<standard-class>"}
-var StandardObject = BuiltInClass.New(Object, "<standard-object>")
-var Stream = BuiltInClass.New(Object, "<stream>")
+var Object = &builtin{[]Instance{}, "<object>"}
+var BasicArray = New(BuiltInClass, Object, "<basic-array>")
+var BasicArrayStar = New(BuiltInClass, BasicArray, "<basic-array*>")
+var GeneralArrayStar = New(BuiltInClass, BasicArrayStar, "<general-array*>")
+var BasicVector = New(BuiltInClass, BasicArray, "<basic-vector>")
+var GeneraVector = New(BuiltInClass, BasicVector, "<general-vector>")
+var String = New(BuiltInClass, BasicVector, "<string>")
+var BuiltInClass = &meta{"<built-in-class>"}
+var Character = New(BuiltInClass, Object, "<character>")
+var Function = New(BuiltInClass, Object, "<function>")
+var GenericFunction = New(BuiltInClass, Function, "<generic-function>")
+var StandardGenericFunction = New(BuiltInClass, GenericFunction, "<standard-generic-function>")
+var List = New(BuiltInClass, Object, "<list>")
+var Cons = New(BuiltInClass, List, "<cons>")
+var Null = New(BuiltInClass, List, "<null>")
+var Symbol = New(BuiltInClass, Object, "<symbol>")
+var Number = New(BuiltInClass, Object, "<number>")
+var Integer = New(BuiltInClass, Number, "<integer>")
+var Float = New(BuiltInClass, Number, "<float>")
+var SeriousCondition = New(BuiltInClass, Object, "<serious-condition>")
+var Error = New(BuiltInClass, SeriousCondition, "<error>")
+var ArithmeticError = New(BuiltInClass, Error, "<arithmetic-error>")
+var DivisionByZero = New(BuiltInClass, ArithmeticError, "<division-by-zero>")
+var FloatingPointOnderflow = New(BuiltInClass, ArithmeticError, "<floating-point-overflow>")
+var FloatingPointUnderflow = New(BuiltInClass, ArithmeticError, "<floating-point-underflow>")
+var ControlError = New(BuiltInClass, Error, "<control-error>")
+var ParseError = New(BuiltInClass, Error, "<parse-error>")
+var ProgramError = New(BuiltInClass, Error, "<program-error>")
+var DomainError = New(BuiltInClass, ProgramError, "<domain-error>")
+var UndefinedEntity = New(BuiltInClass, ProgramError, "<undefined-entity>")
+var UndefinedVariable = New(BuiltInClass, UndefinedEntity, "<undefined-variable>")
+var UndefinedFunction = New(BuiltInClass, UndefinedEntity, "<undefined-function>")
+var SimpleError = New(BuiltInClass, Error, "<simple-error>")
+var StreamError = New(BuiltInClass, Error, "<stream-error>")
+var EndOfStream = New(BuiltInClass, StreamError, "<end-of-stream>")
+var StorageExhausted = New(BuiltInClass, SeriousCondition, "<storage-exhausted>")
+var StandardClass = &meta{"<standard-class>"}
+var StandardObject = New(BuiltInClass, Object, "<standard-object>")
+var Stream = New(BuiltInClass, Object, "<stream>")
