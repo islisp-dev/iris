5e702f3608b84029c0de84c3ea9a5a19163dba0d
asciian <asciian@outlook.jp>
2017-08-11 21:45:44 +0900

    Remove unused functions

--- runtime/ilos/ilos.go
+++ runtime/ilos/ilos.go
@@ -19,31 +19,3 @@ type Instance interface {
 	SetSlotValue(Instance, Instance, Class) bool
 	String() string
 }
-
-func ChildOf(c, p Class) bool {
-	var sub func(c, p Class) bool
-	sub = func(c, p Class) bool {
-		if c == p {
-			return true
-		}
-		for _, d := range c.Parents() {
-			if sub(d, p) {
-				return true
-			}
-		}
-		return false
-	}
-	for _, d := range c.Parents() {
-		if sub(d, p) {
-			return true
-		}
-	}
-	return false
-}
-
-func InstanceOf(i Instance, p Class) bool {
-	if i.Class() == p {
-		return true
-	}
-	return ChildOf(i.Class(), p)
-}
