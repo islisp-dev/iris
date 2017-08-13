34fcda608af5f4739088b842ae45b4574ad57d92
TANIGUCHI Masaya <ta2gch@moffice.space>
2017-08-13 17:05:44 +0900

    Added Eq, Eql, Equal

--- runtime/equality.go
+++ runtime/equality.go
@@ -4,4 +4,43 @@
 
 package runtime
 
-// TODO: eq, eql, equal
+import (
+	"reflect"
+
+	"github.com/ta2gch/iris/runtime/environment"
+	"github.com/ta2gch/iris/runtime/ilos"
+)
+
+// Eq tests whether obj1 and obj2 are same identical object.
+// They return t if the objects are the same; otherwise, they return nil.
+// Two objects are the same if there is no operation that could distinguish
+// them (without modifying them), and if modifying one would modify the other the same way.
+func Eq(_, _ *environment.Environment, obj1, obj2 ilos.Instance) (ilos.Instance, ilos.Instance) {
+	if obj1 == obj2 {
+		return T, nil
+	}
+	return Nil, nil
+}
+
+// Eql tests whether obj1 and obj2 are same identical object.
+// They return t if the objects are the same; otherwise, they return nil.
+// Two objects are the same if there is no operation that could distinguish
+// them (without modifying them), and if modifying one would modify the other the same way.
+func Eql(_, _ *environment.Environment, obj1, obj2 ilos.Instance) (ilos.Instance, ilos.Instance) {
+	if obj1 == obj2 {
+		return T, nil
+	}
+	return Nil, nil
+}
+
+// Equal tests whether obj1 and obj2 are isomorphic—i.e., whether obj1 and obj2 denote the same
+// structure with equivalent values. equal returns t if the test was satisfied, and nil if not.
+// Specifically:
+//
+// If obj1 and obj2 are direct instances of the same class, equal returns t if they are eql.
+func Equal(_, _ *environment.Environment, obj1, obj2 ilos.Instance) (ilos.Instance, ilos.Instance) {
+	if reflect.DeepEqual(obj1, obj2) {
+		return T, nil
+	}
+	return Nil, nil
+}
