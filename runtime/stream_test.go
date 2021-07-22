5607f2741994448fc272ac20ea3ff1f53d3639f9
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-07-22 13:14:11 +0000

    Add test Symbolp

--- runtime/stream_test.go
+++ runtime/stream_test.go
@@ -2,6 +2,20 @@ package runtime
 
 import "testing"
 
+func TestStreamp(t *testing.T) {
+	execTests(t, Streamp, []test{
+		{
+			exp:     `(streamp (standard-input))`,
+			want:    `t`,
+			wantErr: false,
+		},
+		{
+			exp:     `(streamp '())`,
+			want:    `nil`,
+			wantErr: false,
+		},
+	})
+}
 
 func TestWithOpenIoFile(t *testing.T) {
 	execTests(t, WithOpenIoFile, []test{
